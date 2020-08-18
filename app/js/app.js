c1b5308fa18c5633339461685fa5dea40da7ddf8
Serhii Sobol <itsergeysobol@gmail.com>
2020-08-18 15:23:14 +0300

    Add jQuery, Slick slider & section Quotes

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	// Custom JS
+    $('.quotes__slider').slick({
+        // arrows: false,
+        // dots: true,
+        // adaptiveHeight: true,
+        // slidesToShow: 1,
+        // slidesToScroll: 1,
+        // speed: 1000,
+        // easing: 'ease',
+        // autoplay: true,
+        // autoplaySpeed: 1500,
+        // pauseOnFocus: true,
+        // pauseOnHover: true,
+        // mobileFirst: true,
+    });
 
 });
